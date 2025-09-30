a609d20a4cbf470eb8b337b58266d9c7c13d27a9
amnewskells-collab <amnewskells@outlook.com>
2025-09-30 19:10:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 from telegram.ext import Application, CommandHandler
 import os
 
-TOKEN = os.getenv("8462737921:AAGN7uxkA4VpOuyHrMYreq07Fc95_uuR8g4
-")
+TOKEN = os.getenv("8462737921:AAGN7uxkA4VpOuyHrMYreq07Fc95_uuR8g4")
 
 async def start(update, context):
     await update.message.reply_text("Hello! Your Gym Coach bot is alive 🚀")
@@ -11,4 +10,5 @@ if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
 
